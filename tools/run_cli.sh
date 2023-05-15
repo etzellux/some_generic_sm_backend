a6f0e3aa79a82ebbf7894b716308226a489b9782
etzellux <tfslayer@hotmail.com>
2023-05-15 23:04:33 +0300

    init

---
+++ tools/run_cli.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+CONTAINER_NAME="some_generic_sm"
+
+docker exec -it ${CONTAINER_NAME} /bin/bash
---
+++ tools/start_containers.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+CONTAINER_NAME="some_generic_sm"
+DOCKER_FILE="docker-compose.yml"
+
+docker-compose stop
+docker-compose -f ${DOCKER_FILE} rm --force
+docker-compose -f ${DOCKER_FILE} build
+docker-compose -f ${DOCKER_FILE} up -d --remove-orphans
+docker exec -it ${CONTAINER_NAME} /bin/bash
+
